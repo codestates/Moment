69e4431c8b7ce399cae1d81de2bf8682c1e2aa0f
Mangpha <8350130@gmail.co>
2021-08-25 15:53:02 +0900

    [Server] Fix: index.js PORT

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const cookie_parser = require("cookie-parser");
-const PORT = process.env.PORT || 443;
+const PORT = process.env.PORT || 80;
 
 const app = express();
 
