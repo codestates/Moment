9eb3e79e805be9832ec17b364ec9c502c1b3fe71
Mangpha <8350130@gmail.co>
2021-08-25 10:16:07 +0900

    [SR] Project Start

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const cookie_parser = require("cookie-parser");
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(cors());
+app.use(cookie_parser());
+app.use(express.json());
+
+app.listen(PORT, (req, res) => console.log(res));
---
+++ server/routes/index.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+
+const { userController, postController } = require("../controllers");
+
+module.exports = router;
