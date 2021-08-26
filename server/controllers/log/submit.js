7677263db6437bcaab1ae15d1dd0cd9076bdb891
Mangpha <78544031+Mangpha@users.noreply.github.com>
2021-08-26 19:42:10 +0900

    [Server] Add: Fix, Submit (#69)
    * [Server] Add: DB Seed & Fix: DB Schema
    
    * [SR] Add: appspec.yml
    
    * [Server] Add: Server Pipeline
    
    * [Client] Add: buildSpec
    
    * [Server] Fix: Controller/Users
    
    * [Server] Fix: Migration File
    
    * [Server] Fix: Migration Files
    
    * [Server] Fix: Post => Log
    
    * [Server] Fix: Post => Log
    
    * [Server] Fix: routes
    
    * [Server] Add: Submit
    
    * [Server] Add: logs/fix

---
+++ server/controllers/log/submit.js
@@ -0,0 +1,25 @@
+const { Posts } = require('../../models');
+
+module.exports = async (req, res) => {
+	const { title, content, secret } = req.body;
+	const accessToken = req.cookies.accessToken;
+	if (!accessToken) {
+		res.status(401).send({ isSubmit: false });
+	} else {
+		// token verify
+		// const userid = tokenverify(accessToken)
+		if (!userid) {
+			res.status(400).send({ isSubmit: false });
+		} else {
+			await Posts.create({
+				// user_id,
+				title,
+				content,
+				secret,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			});
+			res.status(200).send({ isSubmit: true });
+		}
+	}
+};
