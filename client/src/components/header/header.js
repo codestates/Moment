9e20704fc6710858cfd01d494b1672b9bc9258d5
xinnni <79839230+xinnni@users.noreply.github.com>
2021-08-26 19:42:35 +0900

    [Client] Fix: start page component main svg (#71)
    * [Client] Add: Start page
    
    * [Client] Add: header component
    
    * [Client] Fix: header component css
    
    * [Client] Fix: header component css
    
    * [Client] Fix: start page component main svg

--- client/src/components/header/header.js
+++ client/src/components/header/header.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './header.css';
 
 export default function Header() {
 	return (
 		<div className="header">
-			<h1 className="header-title">Moment.</h1>
+			<Link className="header-title" to="/">
+				<h1>Moment</h1>
+			</Link>
 			<div className="option">
 				<div className="header-login">Login</div>
 				<div className="header-signup">Sign Up</div>
--- client/src/components/start-page/startpage.js
+++ client/src/components/start-page/startpage.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './startpage.css';
+// import { dummyData } from '../../static/dummyData';
 
-import { ReactComponent as Logo } from '../../assets/LovingDoodle.svg';
-
+import { ReactComponent as Logo } from '../../assets/svg/LovingDoodle.svg';
 export default function StartPage() {
+	// const [section, setSection] = useState(dummyData);
+	// console.log(section);
 	return (
 		<div className="startpage">
+			{/* {section.map(({ id, imageUrl }) => (
+				<div className="startpage-logo" key={id} style={{ backgroundImage: `url(${imageUrl})` }} />
+			))} */}
 			<Logo className="startpage-logo" />
 			<div></div>
 			<h1 className="startpage-title">Remember your moment in Moments forever.</h1>
