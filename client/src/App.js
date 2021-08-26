4030413bbcc31a1a4acb964397d2d2e859dc43a4
Mark <notbibu@gmail.com>
2021-08-26 13:24:42 +0900

    [Client] Fix: app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
+import HomePage from '../src/components/pages/homepage';
+import Header from '../src/components/header/header';
 
 function App() {
-
-	return (
-		<div>
-			is it working?
-		</div>
-)
-
+    return <HomePage />;
 }
 
 export default App;
