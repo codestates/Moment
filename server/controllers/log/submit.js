b1d80d72195b95f815ad1fcd51b0a79c0dad4d52
Mangpha <8350130@gmail.co>
2021-08-26 17:18:37 +0900

    [Server] Add: Submit

--- server/controllers/log/submit.js
+++ server/controllers/log/submit.js
@@ -1,3 +1,25 @@
 const { Posts } = require('../../models');
 
-module.exports = async (req, res) => {};
+module.exports = async (req, res) => {
+	const { title, content, secret } = req.body;
+	const accessToken = req.cookies.accessToken;
+	if (!accessToken) {
+		res.status(401).send({ isSubmit: False });
+	} else {
+		// token verify
+		// const userid = tokenverify(accessToken)
+		if (!userid) {
+			res.status(400).send({ isSubmit: False });
+		} else {
+			await Posts.create({
+				// user_id,
+				title,
+				content,
+				secret,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			});
+			res.status(200).send({ isSubmit: True });
+		}
+	}
+};
