f3c6cd965388624f8f3157a381d8c7f6b41c5be1
Mangpha <78544031+Mangpha@users.noreply.github.com>
2021-08-26 13:20:57 +0900

    [Server] Add: DB Seed & Fix: DB Schema (#58)
    [Server] Add: DB Seed & Fix: DB Schema (#58)

---
+++ server/migrations/20210826023036-follower_followee.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		/**
+		 * Add altering commands here.
+		 *
+		 * Example:
+		 * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+		 */
+		await queryInterface.addColumn('follows', 'follower', Sequelize.INTEGER);
+		await queryInterface.addConstraint('follows', {
+			fields: ['follower'],
+			type: 'foreign key',
+			name: 'fk_follower',
+			references: {
+				table: 'users',
+				field: 'id',
+			},
+			onDelete: 'cascade',
+			onUpdate: 'cascade',
+		});
+		await queryInterface.addColumn('follows', 'followee', Sequelize.INTEGER);
+		await queryInterface.addConstraint('follows', {
+			fields: ['followee'],
+			type: 'foreign key',
+			name: 'fk_followee',
+			references: {
+				table: 'users',
+				field: 'id',
+			},
+			onDelete: 'cascade',
+			onUpdate: 'cascade',
+		});
+	},
+
+	down: async (queryInterface, Sequelize) => {
+		/**
+		 * Add reverting commands here.
+		 *
+		 * Example:
+		 * await queryInterface.dropTable('users');
+		 */
+		await queryInterface.removeConstraint('follows', 'fk_follower');
+		await queryInterface.removeColumn('follows', 'follower');
+		await queryInterface.removeConstraint('follows', 'fk_followee');
+		await queryInterface.removeColumn('follows', 'followee');
+	},
+};
