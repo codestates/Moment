8e0220f21702459ff9b1010e1963fd735441f83f
HTlee1990 <notbibu@gmail.com>
2021-08-27 16:54:19 +0900

    [Client]Add: Log Component, secret (#89)
    * [Client]Fix: Login component
    
    * [Client]Add: Conditional Input Color Change
    
    * [Client]Fix: Login, Google Component
    
    * [Client]Add: make SignUp Login Modal closed
    
    * [Client]Add: Write Component & Login animation CSS
    
    * [Client]Add: Write, Mypage, MypageDetail Component
    
    * [Client]Add: Log, MyLog, Mypage, MyPageDetail Component etc
    
    * [Client]Add: Log Component && Fix: Mypage, Mypage Detail
    
    * [Client]Add: Log Component && Fix: Mypage, Mypage Detail
    
    * [Client]Add: Log Component && Fix: Mypage, Mypage Detail

--- client/src/components/UI/Input.js
+++ client/src/components/UI/Input.js
@@ -2,9 +2,10 @@ import React, {useEffect} from 'react'
 import PropTypes from 'prop-types'
 import classes from './Input.module.css'
 
-const Input = ({children, input, className}) => {
+const Input = ({children, input, className }) => {
     return (
         <div className={classes.container}>
+            {/* <label htmlFor={input.id}> {label} </label> */}
             <input className={`${classes.input} ${className}`}  {...input}>
             {children} 
             </input>
@@ -15,7 +16,8 @@ const Input = ({children, input, className}) => {
 Input.propTypes = {
     children: PropTypes.any,
     input: PropTypes.any,
-    className: PropTypes.any
+    className: PropTypes.any,
+    label: PropTypes.any
 }
 
 export default Input
