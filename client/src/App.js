99e7f7954383ed031b550f167af54cf777a87ce0
xinnni <79839230+xinnni@users.noreply.github.com>
2021-08-27 12:30:24 +0900

    [Client] Fix: component

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
+
 import HomePage from '../src/components/pages/homepage';
 import Header from '../src/components/header/header';
 import Login from './components/login/Login';
 import Log from './components/log/Log'
 import Mypage from './components/mypage/Mypage'
 import MypageDetail from './components/mypage/MypageDetail'
+
 
 function App() {
 	return (
