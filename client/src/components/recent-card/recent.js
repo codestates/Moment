1a2d45ac60fadf3e6c48fa7a76c610afaf125285
xinnni <79839230+xinnni@users.noreply.github.com>
2021-08-27 16:46:00 +0900

    [Client] Add: Spinner page and recent card (#85)
    * [Client] Add: Start page
    
    * [Client] Add: header component
    
    * [Client] Fix: header component css
    
    * [Client] Fix: header component css
    
    * [Client] Fix: start page component main svg
    
    * [Client] Fix: start page component CSS
    
    * [Client] Fix: main page component CSS and added recent card page component
    
    * [Client] Fix: main page layout
    
    * [Client] Fix: main page component layout
    
    * [Clinet] Fix: component
    
    * [Client] Fix: component
    
    * [Client] Fix: component
    
    * [Client] Add: Spinner component
    
    * [client] Add: Recent card component
    
    * [Client] Add: Spinner page and recent card

--- client/src/components/recent-card/recent.js
+++ client/src/components/recent-card/recent.js
@@ -3,13 +3,88 @@ import PropTypes from 'prop-types';
 
 import './recent.css';
 
-const RecentCard = imageUrl => {
-	console.log(imageUrl.imageUrl);
-	return <img src={imageUrl.imageUrl} />;
+const RecentCard = ({ title, body }) => {
+	return (
+		<div className="contains">
+			<div className="recent-container">
+				<div className="recent-image-container">
+					<img src={require('../../static/images/composition-10.png').default} />
+				</div>
+				<div className="recent-card-container">
+					<div className="recent-title">
+						<h1>{title}</h1>
+					</div>
+					<div className="recent-body">
+						<p>{body}</p>
+					</div>
+				</div>
+				<div className="recent-btn-container">
+					<button className="recent-btn">
+						<a>View more</a>
+					</button>
+				</div>
+			</div>
+			<div className="recent-container">
+				<div className="recent-image-container">
+					<img src={require('../../static/images/composition-11.png').default} />
+				</div>
+				<div className="recent-card-container">
+					<div className="recent-title">
+						<h1>{title}</h1>
+					</div>
+					<div className="recent-body">
+						<p>{body}</p>
+					</div>
+				</div>
+				<div className="recent-btn-container">
+					<button className="recent-btn">
+						<a>View more</a>
+					</button>
+				</div>
+			</div>
+			<div className="recent-container">
+				<div className="recent-image-container">
+					<img src={require('../../static/images/composition-21.png').default} />
+				</div>
+				<div className="recent-card-container">
+					<div className="recent-title">
+						<h1>{title}</h1>
+					</div>
+					<div className="recent-body">
+						<p>{body}</p>
+					</div>
+				</div>
+				<div className="recent-btn-container">
+					<button className="recent-btn">
+						<a>View more</a>
+					</button>
+				</div>
+			</div>
+			<div className="recent-container">
+				<div className="recent-image-container">
+					<img src={require('../../static/images/composition-22.png').default} />
+				</div>
+				<div className="recent-card-container">
+					<div className="recent-title">
+						<h1>{title}</h1>
+					</div>
+					<div className="recent-body">
+						<p>{body}</p>
+					</div>
+				</div>
+				<div className="recent-btn-container">
+					<button className="recent-btn">
+						<a>View more</a>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
-RecentCard.PropTypes = {
-	imageUrl: PropTypes.any,
+RecentCard.propTypes = {
+	title: PropTypes.any,
+	body: PropTypes.any,
 };
 
 export default RecentCard;
