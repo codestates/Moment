3637e8bc6451d62ae4fe6794b3006d87cf8d8aa3
xinnni <79839230+xinnni@users.noreply.github.com>
2021-08-27 14:30:16 +0900

    [Client] Add: Spinner component

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,13 @@ import './App.css';
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
-
 import HomePage from '../src/components/pages/homepage';
 import Header from '../src/components/header/header';
 import Login from './components/login/Login';
-import Log from './components/log/Log'
-import Mypage from './components/mypage/Mypage'
-import MypageDetail from './components/mypage/MypageDetail'
-
+import Log from './components/log/Log';
+import Mypage from './components/mypage/Mypage';
+import MypageDetail from './components/mypage/MypageDetail';
+import MainPage from '../src/components/main-page/mainpage';
 
 function App() {
 	return (
@@ -18,6 +17,9 @@ function App() {
 			<Switch>
 				<Route exact path="/">
 					<HomePage />
+				</Route>
+				<Route exact path="/main">
+					<MainPage />
 				</Route>
 				<Route path="/log">
 					<Log />
