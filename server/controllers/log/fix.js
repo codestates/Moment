2570f7e1a005bc419a96f7b15f741ee5413f204f
lotus0204 <lotusrb0204@gmail.com>
2021-08-27 12:06:13 +0900

    [Server]fix:fix,submit

--- server/controllers/log/fix.js
+++ server/controllers/log/fix.js
@@ -1,27 +1,29 @@
 const { Posts } = require('../../models');
-const { isAuthorized } = require('../../lib/index');
 
 module.exports = async (req, res) => {
 	const post_id = req.params.id;
 	const accessToken = req.cookies.accessToken;
 	const { title, content } = req.body;
+	const findPost = await Posts.findOne({ where: { id: post_id } });
+	if (!findPost) res.status(404).send({ isFixed: false });
 	if (!accessToken) {
 		res.status(401).send({ isFixed: false });
 	} else {
-		// const userid = isAuthorized(accessToken);
-		const accessTokenData = isAuthorized(accessToken);
-		const userInfo = await Users.findOne({
-			where: { email: accessTokenData.email }
-		});
-		if (!userInfo) {
-			res.status(400).send({ isFixed: false });
-		} else {
-			const origin = await Posts.findOne({ where: { user_id: userInfo.id, id: post_id } });
+		// token verify
+		// const userid = tokenverify(accessToken)
+		// if (!userid) {
+		// 	res.status(400).send({ isFixed: false });
+		// } else {
+		try {
+			const origin = await Posts.findOne({ where: { user_id: userid, id: post_id } });
 			await Posts.update({
 				title: title ? title : origin.title,
 				content: content ? content : origin.content,
 			});
 			res.status(200).send({ isFixed: true });
+		} catch (err) {
+			console.log(err);
 		}
+		// }
 	}
 };
--- server/controllers/log/submit.js
+++ server/controllers/log/submit.js
@@ -1,5 +1,4 @@
 const { Posts } = require('../../models');
-const { isAuthorized } = require('../../lib/index');
 
 module.exports = async (req, res) => {
 	const { title, content, secret } = req.body;
@@ -7,24 +6,24 @@ module.exports = async (req, res) => {
 	if (!accessToken) {
 		res.status(401).send({ isSubmit: false });
 	} else {
-		const accessTokenData = isAuthorized(accessToken);
-		const userInfo = await Users.findOne({
-			where: { email: accessTokenData.email }
-		});
-		// const userid = isAuthorized(accessToken);
-		// accessToken이 바로 userid가 되진 않는것 같은데...
-		if (!userInfo) {
+		// token verify
+		// const userid = tokenverify(accessToken)
+		if (!userid) {
 			res.status(400).send({ isSubmit: false });
 		} else {
-			await Posts.create({
-				user_id: userInfo.id,
-				title,
-				content,
-				secret,
-				createdAt: new Date(),
-				updatedAt: new Date(),
-			});
-			res.status(200).send({ isSubmit: true });
+			try {
+				await Posts.create({
+					// user_id,
+					title,
+					content,
+					secret,
+					createdAt: new Date(),
+					updatedAt: new Date(),
+				});
+				res.status(200).send({ isSubmit: true });
+			} catch (err) {
+				console.log(err);
+			}
 		}
 	}
 };
