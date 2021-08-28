1b38599310e5499cd26aecf7f07e046e95276d34
xinnni <79839230+xinnni@users.noreply.github.com>
2021-08-28 23:33:50 +0900

    [Client] Add: Recent page, main page preview card

--- client/src/components/post-card/postcard.js
+++ client/src/components/post-card/postcard.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import SideImage from './sideimage';
+import Detail from './detail';
+
 import './postcard.css';
 
 export default function PostCard() {
-	return <div className="post-card-container">card</div>;
+	return (
+		<div className="post-card-container">
+			<Detail />
+			<SideImage />
+		</div>
+	);
 }
