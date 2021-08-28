155f086b0c8dd48385d9f16db87d65160e569fb4
xinnni <79839230+xinnni@users.noreply.github.com>
2021-08-29 00:31:06 +0900

    [Client] Add: My post, recent page component

---
+++ client/src/components/pages/recentpage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './recentpage.css';
+
+import PostCard from '../post-card/postcard';
+
+export default function RecentPage() {
+	return (
+		<div className="recent-main-container">
+			<PostCard />
+		</div>
+	);
+}
