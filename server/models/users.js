fd9cfbc5899c6a5a4c25932b8ff26c1dc41f9adc
Mangpha <8350130@gmail.co>
2021-08-28 17:16:00 +0900

    [Server] Fix: Models

--- server/models/users.js
+++ server/models/users.js
@@ -9,11 +9,11 @@ module.exports = (sequelize, DataTypes) => {
 		 */
 		static associate(models) {
 			// define association here
-			models.Users.hasMany(models.Posts, { foreignKey: 'user_id' });
-			models.Users.hasMany(models.post_like, { foreignKey: 'user_id' });
-			models.Users.hasMany(models.follow, { foreignKey: 'follower' });
-			models.Users.hasMany(models.follow, { foreignKey: 'followee' });
-			models.Users.hasMany(models.Comment, { foreignKey: 'comment_userid' });
+			models.Users.hasMany(models.Posts);
+			models.Users.hasMany(models.post_like);
+			models.Users.hasMany(models.follow);
+			models.Users.hasMany(models.follow);
+			models.Users.hasMany(models.Comment);
 		}
 	}
 	Users.init(
