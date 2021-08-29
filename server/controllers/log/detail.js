70b2318b98d878bcadec6649eb74b62b0f2019b1
Mangpha <8350130@gmail.co>
2021-08-29 15:53:09 +0900

    [Server] Fix: Detail, Like, Models

--- server/controllers/log/detail.js
+++ server/controllers/log/detail.js
@@ -3,21 +3,19 @@ const { Posts, Users } = require('../../models');
 module.exports = async (req, res) => {
 	const post_id = req.params.id;
 	try {
-		const post = await Posts.findOne({ where: { id: post_id } });
-		const userInfo = await Users.findOne({
-			where: { id: post.user_id },
-		});
+		const post = await Posts.findOne({ where: { id: post_id, secret: false } });
+		const userInfo = await Users.findByPk(post.user_id);
 		if (!post) {
 			res.status(404).json({ data: null });
 		} else {
-			const { id, title, content, updated_at, like_count } = post;
+			const { id, title, content, updatedAt, like_count } = post;
 			res.status(200).json({
 				data: {
-					id: id,
-					title: title,
-					content: content,
+					id,
+					title,
+					content,
 					author: userInfo.nickname,
-					updated: updated_at,
+					updated: updatedAt,
 					like: like_count,
 				},
 			});
