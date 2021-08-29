91980984ce7a3d14c2e7a0c923b7de971e9e8aa9
xinnni <79839230+xinnni@users.noreply.github.com>
2021-08-29 21:49:11 +0900

    [Client] Add: context.js

--- client/src/components/main-page/mainpage.js
+++ client/src/components/main-page/mainpage.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
-
 import RecentCard from '../recent-card/recent';
 import Spinner from '../spinner/spinner';
-
 import './mainpage.css';
+import { Context } from '../../Context';
 
 export default function MainPage() {
+	const { posts } = useContext(Context);
 	const [title, setTitle] = useState('dummy');
 	const [content, setContent] = useState(
 		'Thank you. Thank you so much. I am so thrilled to be recieving this award today. But on the other hand, it`s a little bit bittersweet. It`s a pitty that the musical arts are evaluated and ranked like this. Yes. I know that this is too wonderful and glorious for me. I don`t want to denigrate this position or award. Thank you again for giving my this award.',
@@ -27,10 +27,6 @@ export default function MainPage() {
 
 	if (!isLoading) return <Spinner />;
 
-	// useEffect(() => {
-	// 	axios.get(`https://api.m0ment.be/log/recent/page/${num}`);
-	// });
-
 	return (
 		<>
 			<div className="mainpage">
@@ -45,9 +41,12 @@ export default function MainPage() {
 				<RecentCard title={title} content={content} author={author} updated={updated} />
 				<RecentCard title={title} content={content} author={author} updated={updated} />
 			</div>
-			{/* <div>
-				<button>View all</button>
-			</div> */}
+
+			<div className="mainpage-recent-btn-container">
+				<Link to="/main/recent">
+					<button className="recent-btn">View more</button>
+				</Link>
+			</div>
 		</>
 	);
 }
