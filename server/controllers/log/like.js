ea7e0acf57ec6d908641e5415cd3c746099e88c6
Mangpha <8350130@gmail.co>
2021-08-29 12:51:42 +0900

    [Server] Fix: Conflict

--- server/controllers/log/like.js
+++ server/controllers/log/like.js
@@ -2,41 +2,41 @@ const { Posts, Users, post_like } = require('../../models');
 const { isAuthorized } = require('../../lib/index');
 
 module.exports = async (req, res) => {
-	const post_id = req.params.id;
-	const accessToken = req.cookies.accessToken;
-	if (!accessToken) {
-		res.status(400).send({ isLike: false });
-	} else {
-		const accessTokenData = isAuthorized(accessToken);
-		const userId = await Users.findOne({
-			where: { email: accessTokenData.email, nickname: accessTokenData.nickname },
-		});
-		if (!userId) {
-			res.status(401).send({ isLike: false });
-		} else {
-			const postId = await Posts.findOne({
-				where: { id: post_id },
-			});
-			const isLike = await post_like.findOne({
-				where: {
-					post_id: postId.id,
-					user_id: userId.id,
-				},
-			});
-			if (!isLike) {
-				await post_like.create({
-					post_id: postId.id,
-					user_id: userId.id,
-				});
-				res.status(200).json({ isLike: true });
-			} else {
-				await post_like.destroy({
-					post_id: postId.id,
-					user_id: userId.id,
-				});
-				res.status(200).json({ isLike: false });
-			}
-      
-		}
-	}
-};
+  const post_id = req.params.id;
+  const accessToken = req.cookies.accessToken;
+  if (!accessToken) {
+    res.status(400).send({ isLike: false });
+  } else {
+    const accessTokenData = isAuthorized(accessToken);
+    const userId = await Users.findOne({
+      where: { email: accessTokenData.email, nickname: accessTokenData.nickname }
+    });
+    if (!userId) {
+      res.status(401).send({ isLike: false });
+    } else {
+      const postId = await Posts.findOne({
+        where: { id: post_id }
+      });
+      const isLike = await post_like.findOne({
+        where: {
+          post_id: postId.id,
+          user_id: userId.id
+        }
+      });
+      if (!isLike) {
+        await post_like.create({
+          post_id: postId.id,
+          user_id: userId.id
+        });
+        res.status(200).json({ isLike: true });
+      } else {
+        await post_like.destroy({
+          post_id: postId.id,
+          user_id: userId.id
+        });
+        res.status(200).json({ isLike: false });
+      }
+
+    }
+  }
+}
