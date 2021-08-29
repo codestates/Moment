51fc3af2a04a153c8480fb6749d033d3539a764a
xinnni <79839230+xinnni@users.noreply.github.com>
2021-08-29 16:13:38 +0900

    [Client] add: recent page component (#128)
    * [Client] Add: Start page
    
    * [Client] Add: header component
    
    * [Client] Fix: header component css
    
    * [Client] Fix: header component css
    
    * [Client] Fix: start page component main svg
    
    * [Client] Fix: start page component CSS
    
    * [Client] Fix: main page component CSS and added recent card page component
    
    * [Client] Fix: main page layout
    
    * [Client] Fix: main page component layout
    
    * [Clinet] Fix: component
    
    * [Client] Fix: component
    
    * [Client] Fix: component
    
    * [Client] Add: Spinner component
    
    * [client] Add: Recent card component
    
    * [Client] Add: Spinner page and recent card
    
    * [Client] Add: Footer component and css
    
    * [Client] Fix: main page title button css
    
    * [Client] Fix: main page title css and linked log page
    
    * [Client] Add: modal component
    
    * [Client] Fix: modal component icon
    
    * [Client] Fix: css
    
    * [Client] add: Recent page component
    
    * [Client] Fix: css
    
    * [Client] Add: Recent page, main page preview card
    
    * [Client] Add: My post, recent page component
    
    * [Client] Add: profile component demo and post card component fixed
    
    * [Client] Fix: recent main page
    
    * [Client] Fix: recent main page

--- client/src/App.js
+++ client/src/App.js
@@ -11,32 +11,42 @@ import MypageDetail from './components/mypage/MypageDetail';
 import MainPage from '../src/components/main-page/mainpage';
 import PostCard from '../src/components/post-card/postcard';
 import Modal from './components/modal/Modal';
-import LogDetail from './components/log/LogDetail'
-import axios from 'axios'
+import LogDetail from './components/log/LogDetail';
+import RecentPage from '../src/components/pages/recentpage';
+import axios from 'axios';
 
 function App() {
 	const [userInfo, setUserInfo] = useState('');
 	const [login, setLogin] = useState(false);
-	const [refreshToken, setRefreshToken] = useState('')
-	const getUserInfo = (data) => {
+	const [refreshToken, setRefreshToken] = useState('');
+	const getUserInfo = data => {
 		setUserInfo(data);
 		console.log('userInfo at App :', data);
 	};
 	const loginHandler = () => {
-		setLogin(true)
-	}
-	const refreshTokenHandler = (token) => {
-		setRefreshToken(token)
-	}
+		setLogin(true);
+	};
+	const refreshTokenHandler = token => {
+		setRefreshToken(token);
+	};
 	const logoutHandler = async () => {
-		let header = {refreshToken: `${refreshToken}`}
-		const response = await axios.get('https://api.m0ment.be/users/logout', {withCredentials: true, headers: header})
+		let header = { refreshToken: `${refreshToken}` };
+		const response = await axios.get('https://api.m0ment.be/users/logout', {
+			withCredentials: true,
+			headers: header,
+		});
 		console.log(response);
-		setLogin(false)
-	}
+		setLogin(false);
+	};
 	return (
 		<div>
-			<Header getUserInfo={getUserInfo} loginHandler={loginHandler} login={login} logoutHandler={logoutHandler} refreshTokenHandler={refreshTokenHandler}/>
+			<Header
+				getUserInfo={getUserInfo}
+				loginHandler={loginHandler}
+				login={login}
+				logoutHandler={logoutHandler}
+				refreshTokenHandler={refreshTokenHandler}
+			/>
 			<Switch>
 				<Route exact path="/">
 					<HomePage />
@@ -45,16 +55,19 @@ function App() {
 					<MainPage />
 				</Route>
 				<Route path="/log">
-					<WriteLog login={login} loginHandler={loginHandler}/>
+					<WriteLog login={login} loginHandler={loginHandler} />
 				</Route>
 				<Route path="/myprofile">
-					<Mypage login={login} loginHandler={loginHandler}/>
+					<Mypage login={login} loginHandler={loginHandler} />
 				</Route>
 				<Route path="/fixprofile">
-					<MypageDetail login={login} loginHandler={loginHandler}/>
+					<MypageDetail login={login} loginHandler={loginHandler} />
 				</Route>
 				<Route path="/test">
 					<LogDetail />
+				</Route>
+				<Route path="/main/recent">
+					<RecentPage />
 				</Route>
 			</Switch>
 		</div>
