67c95d0bcc345d611831d2ca62f2a1c6db832577
xinnni <79839230+xinnni@users.noreply.github.com>
2021-08-29 22:56:31 +0900

    [Client] Fix: My profile css fix  (#133)
    * [Client] Add: Start page
    
    * [Client] Add: header component
    
    * [Client] Fix: header component css
    
    * [Client] Fix: header component css
    
    * [Client] Fix: start page component main svg
    
    * [Client] Fix: start page component CSS
    
    * [Client] Fix: main page component CSS and added recent card page component
    
    * [Client] Fix: main page layout
    
    * [Client] Fix: main page component layout
    
    * [Clinet] Fix: component
    
    * [Client] Fix: component
    
    * [Client] Fix: component
    
    * [Client] Add: Spinner component
    
    * [client] Add: Recent card component
    
    * [Client] Add: Spinner page and recent card
    
    * [Client] Add: Footer component and css
    
    * [Client] Fix: main page title button css
    
    * [Client] Fix: main page title css and linked log page
    
    * [Client] Add: modal component
    
    * [Client] Fix: modal component icon
    
    * [Client] Fix: css
    
    * [Client] add: Recent page component
    
    * [Client] Fix: css
    
    * [Client] Add: Recent page, main page preview card
    
    * [Client] Add: My post, recent page component
    
    * [Client] Add: profile component demo and post card component fixed
    
    * [Client] Fix: recent main page
    
    * [Client] Fix: recent main page
    
    * [Client] Add: context.js
    
    * [Client] Fix: color
    
    * [Client] Fix: css
    
    * [Client] Fix: css

--- client/src/components/mypage/MypageDetail.js
+++ client/src/components/mypage/MypageDetail.js
@@ -1,67 +1,107 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import classes from './MypageDetail.module.css';
 import PropTypes from 'prop-types';
 import Card from '../UI/MypageCard';
-import Input from '../UI/Input'
-import Button from '../UI/Button'
-import Modal from '../modal/Modal'
+import Input from '../UI/Input';
+import Button from '../UI/Button';
+import Modal from '../modal/Modal';
 
+const MypageDetail = ({ login, loginHandler }) => {
+	const [userInfo, setUserInfo] = useState({ email: 'clover@gmail.com', nickname: 'clover' });
+	const [enteredEmail, setEnteredEmail] = useState('clover@gmail.com');
+	const [enteredNickname, setEnteredNickname] = useState('clover');
+	const [enteredPassword, setEnteredPassword] = useState('');
+	const [passwordIsValid, setPasswordIsValid] = useState(true);
+	const [confirmPassword, setConfirmPassword] = useState(true);
 
-const MypageDetail = ({login, loginHandler}) => {
-    const [userInfo, setUserInfo] = useState({email:'clover@gmail.com', nickname:'clover'});
-    const [enteredEmail, setEnteredEmail] = useState('clover@gmail.com');
-    const [enteredNickname, setEnteredNickname] = useState('clover')
-    const [enteredPassword, setEnteredPassword] = useState('');
-    const [passwordIsValid, setPasswordIsValid] = useState(true)
-    const [confirmPassword, setConfirmPassword] = useState(true);
+	const editHandler = event => {
+		event.preventDefault();
+		axios
+			.post('https://api.m0ment.be/', { email: '', nickname: '', password: '' }, { withCredentials: true })
+			.then(res => console.log(res));
+	};
+	const emailInputHandler = e => {
+		setEnteredEmail(e.target.value);
+	};
+	const nickNameInputHandler = e => {
+		setEnteredNickname(e.target.value);
+	};
+	const passwordInputHandler = e => {
+		setEnteredPassword(e.target.value);
+	};
+	const validatePasswordHandler = e => {
+		const reg_pw = /^.*(?=.{8,20})(?=.*[0-9])(?=.*[a-zA-Z]).*$/;
+		if (!reg_pw.test(e.target.value)) setPasswordIsValid(false);
+		else setPasswordIsValid(true);
+	};
+	const confirmPasswordHandler = e => {
+		if (enteredPassword === e.target.value) setConfirmPassword(true);
+		else setConfirmPassword(false);
+	};
 
-    const editHandler = (event) => {
-        event.preventDefault();
-        axios.post('https://api.m0ment.be/', {email: '', nickname: '', password: ''}, {withCredentials: true})
-        .then(res => console.log(res))
-    }
-    const emailInputHandler = (e) => {
-        setEnteredEmail(e.target.value)
-    }
-    const nickNameInputHandler = (e) => {
-        setEnteredNickname(e.target.value)
-    }
-    const passwordInputHandler = (e) => {
-        setEnteredPassword(e.target.value)
-    }
-    const validatePasswordHandler = (e) => {
-        const reg_pw = /^.*(?=.{8,20})(?=.*[0-9])(?=.*[a-zA-Z]).*$/
-        if(!reg_pw.test(e.target.value)) setPasswordIsValid(false);
-        else setPasswordIsValid(true);
-    }
-    const confirmPasswordHandler = (e) => {
-        if(enteredPassword === e.target.value) setConfirmPassword(true)
-        else setConfirmPassword(false)
-    }
-
-    return (<>
-        {!login && <Modal loginHandler={loginHandler}/>}
-        {login && 
-        <Card className={classes.container}>
-            <form className={classes.form} onSubmit={editHandler}>
-                <h2 className={classes.title}>My Info</h2>
-                <Input className={classes.ipt} input={{type: "text", placeholder: "E-Mail", value: enteredEmail, onChange: emailInputHandler}}/>
-                <Input className={classes.ipt} input={{type: "text", placeholder: "Nickname", value: enteredNickname, onChange: nickNameInputHandler}}/>
-                <Input className={passwordIsValid ? `${classes.ipt}` : `${classes.ipt} ${classes.invalid}`} input={{placeholder:"Password", type:"password", onChange: passwordInputHandler, onBlur: validatePasswordHandler, value: enteredPassword}} />
-                <Input className={confirmPassword ? `${classes.ipt}` : `${classes.ipt} ${classes.invalid}`} input={{type: "password", placeholder: "Confrim Password", onChange:confirmPasswordHandler}}/>
-                <div className={!confirmPassword ? `${classes.err}` : `${classes.err} ${classes.errNone}`}>비밀번호를 확인 해주세요.</div>
-                <div className={classes.btncontainer}>
-                    <Button className={classes.btn}>Edit</Button>
-                </div>
-            </form>
-        </Card>}
-    </>)
-}
+	return (
+		<>
+			{!login && <Modal loginHandler={loginHandler} />}
+			{login && (
+				<Card className={classes.container}>
+					<div className={classes.form} onSubmit={editHandler}>
+						<h2 className={classes.title}>Information</h2>
+						<Input
+							className={classes.ipt}
+							input={{
+								type: 'text',
+								placeholder: 'E-Mail',
+								value: enteredEmail,
+								onChange: emailInputHandler,
+							}}
+						/>
+						<Input
+							className={classes.ipt}
+							input={{
+								type: 'text',
+								placeholder: 'Nickname',
+								value: enteredNickname,
+								onChange: nickNameInputHandler,
+							}}
+						/>
+						<Input
+							className={passwordIsValid ? `${classes.ipt}` : `${classes.ipt} ${classes.invalid}`}
+							input={{
+								placeholder: 'Password',
+								type: 'password',
+								onChange: passwordInputHandler,
+								onBlur: validatePasswordHandler,
+								value: enteredPassword,
+							}}
+						/>
+						<Input
+							className={confirmPassword ? `${classes.ipt}` : `${classes.ipt} ${classes.invalid}`}
+							input={{
+								type: 'password',
+								placeholder: 'Confrim Password',
+								onChange: confirmPasswordHandler,
+							}}
+						/>
+						<div className={!confirmPassword ? `${classes.err}` : `${classes.err} ${classes.errNone}`}>
+							Please check the password.
+						</div>
+						<div className={classes.imgcontainer}>
+							<img className={classes.imgsize} src={require('../../assets/svg/20.svg').default} />
+						</div>
+						<div className={classes.btncontainer}>
+							<Button className={classes.btn}>Edit</Button>
+						</div>
+					</div>
+				</Card>
+			)}
+		</>
+	);
+};
 
 MypageDetail.propTypes = {
-    loginModalHandler: PropTypes.any,
-    loginOn: PropTypes.any
-}
+	loginModalHandler: PropTypes.any,
+	loginOn: PropTypes.any,
+};
 
-export default MypageDetail
+export default MypageDetail;
--- client/src/components/recent-card/recent.js
+++ client/src/components/recent-card/recent.js
@@ -1,13 +1,8 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
-
 import { Link } from 'react-router-dom';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
-import LogDetail from '../log/LogDetail';
 import './recent.css';
-
 const RecentCard = ({ title, content, author, updated }) => {
 	const [click, setClick] = useState(false);
 	const [count, setCount] = useState(Number(1));
@@ -25,41 +20,34 @@ const RecentCard = ({ title, content, author, updated }) => {
 	};
 
 	return (
-		<div className="contains">
-			<div className="recent-container">
-				<div className="recent-image-container">
-					<img src={require('../../static/images/composition-10.png').default} />
-				</div>
-				<div className="recent-card-container">
-					<div className="recent-title">
-						<h1>{title}</h1>
-						<span className="recent-card-nickname">{author}</span>
-						<span className="recent-card-updete">{updated}</span>
-						<button
-							className={click ? `recent-card-liked-click` : 'recent-card-liked'}
-							onClick={clickHandler}
-						>
-							<FontAwesomeIcon icon={faHeart} />
-						</button>
-						{/* "recent-card-liked" */}
+		<>
+			<div className="contains">
+				<div className="recent-container">
+					<div className="recent-image-container">
+						<img src={require('../../static/images/composition-10.png').default} />
 					</div>
-					<div className="recent-body">
-						<p>{content}</p>
+					<div className="recent-card-container">
+						<div className="recent-title">
+							<h1>{title}</h1>
+							<span className="recent-card-nickname">{author}</span>
+							<span className="recent-card-updete">{updated}</span>
+							<button
+								className={click ? `recent-card-liked-click` : 'recent-card-liked'}
+								onClick={clickHandler}
+							>
+								<FontAwesomeIcon icon={faHeart} />
+							</button>
+							{/* "recent-card-liked" */}
+						</div>
+						<div className="recent-body">
+							<p>{content}</p>
+						</div>
 					</div>
-				</div>
-				<div className="recent-btn-container">
-					<Link to="/main/recent">
-						<button className="recent-btn">View more</button>
-					</Link>
+					<div className="recent-btn-container"></div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
-};
-
-RecentCard.propTypes = {
-	title: PropTypes.any,
-	body: PropTypes.any,
 };
 
 export default RecentCard;
