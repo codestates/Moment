ab0769f9e7721dd5bcb7dd9fce493efdede50f1a
Mangpha <8350130@gmail.co>
2021-08-29 23:03:35 +0900

    [Server] Fix: Conflict

--- server/controllers/log/recent.js
+++ server/controllers/log/recent.js
@@ -1,18 +1,18 @@
 const { Posts, Users } = require('../../models');
 
 module.exports = async (req, res) => {
-	const num = req.params.num || 1;
-	let offset = 4 * (num - 1);
-	try {
-		const pageData = await Posts.findAll({
-			offset: offset,
-			limit: 4,
-			attributes: ['id', 'title', 'content', 'updatedAt', 'like_count', 'user_id'],
-			where: { secret: false },
-			include: [{ model: Users, attributes: ['nickname'] }],
-		});
-		res.status(200).json({ data: pageData });
-	} catch (err) {
-		console.log(err);
-	}
+  const num = req.params.num || 1;
+  let offset = 4 * (num - 1);
+  try {
+    const pageData = await Posts.findAll({
+      offset: offset,
+      limit: 4,
+      attributes: ['id', 'title', 'content', 'updatedAt', 'like_count', 'user_id'],
+      where: { secret: false },
+      include: [{ model: Users, attributes: ['nickname'] }],
+    });
+    res.status(200).json({ data: pageData });
+  } catch (err) {
+    console.log(err);
+  }
 };
