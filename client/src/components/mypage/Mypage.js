0847c328f6f733cd387a7280cc00c15f73b31ba8
xinnni <79839230+xinnni@users.noreply.github.com>
2021-08-30 17:47:41 +0900

    [Client] Fix: info component css

--- client/src/components/mypage/Mypage.js
+++ client/src/components/mypage/Mypage.js
@@ -1,81 +1,43 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import classes from './Mypage.module.css';
-import MypageDetail from './MypageDetail';
-import { AiOutlineRead, AiOutlineFileSearch, AiOutlinePlusCircle } from 'react-icons/ai';
+import './Mypage.css';
+import { FiPaperclip } from 'react-icons/fi';
 import { RiUserSettingsLine } from 'react-icons/ri';
-import { ReactComponent as Book } from '../../assets/book.svg';
-import { ReactComponent as Search } from '../../assets/search.svg';
-import { ReactComponent as Edit } from '../../assets/edit.svg';
-import { ReactComponent as Plus } from '../../assets/plusIcon.svg';
 import { Link } from 'react-router-dom';
 
 const Mypage = () => {
-	const [userInfo, setUserInfo] = useState({ email: '', nickname: '' });
-	useEffect(() => {
-		getUsersInfo();
-	}, []);
-	const getUsersInfo = async () => {
-		const user = await axios.get('https://api.m0ment.be/users/profile', { withCredentials: true });
-		const { email, nickname } = user.data.data;
-		setUserInfo({ email, nickname });
-	};
-	// const { data: userInfo } = res;
-	// setUserInfo(userInfo);
+	const [userInfo, setUserInfo] = useState({ email: 'test@test.com', nickname: 'test' });
+	// useEffect(() => {
+	// 	getUsersInfo();
+	// }, []);
+	// const getUsersInfo = async () => {
+	// 	const user = await axios.get('https://api.m0ment.be/users/profile', { withCredentials: true });
+	// 	const { email, nickname } = user.data.data;
+	// 	setUserInfo({ email, nickname });
+	// };
 
 	const { email, nickname } = userInfo;
-	// console.log(email);
 	return (
-		<>
-			<div className={`${classes.card} ${classes.middle}`}>
-				<div className={classes.front}>
-					<img src="https://images.unsplash.com/photo-1527960669566-f882ba85a4c6?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8YXdlc29tZSUyMHBpY3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80" />
-				</div>
-
-				<div className={classes.back}>
-					<div className={`${classes.back__content} ${classes.middle}`}>
-						<div className={classes.upperContainer}>
-							<div>
-								<img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80" />
-							</div>
-							<Link to="/log">
-								{/* <div className={classes.writeLog}> */}
-								<AiOutlinePlusCircle size={30} color="#2c3e50" />
-								{/* </div> */}
-							</Link>
-							<div className={classes.text__container}>
-								<span>{nickname}</span>
-								<span>{email}</span>
-							</div>
-						</div>
-
-						<div className={classes.link__container}>
-							{/* <Link to="/recent/page/1"> */}
-							<button className={classes.btn}>
-								<AiOutlineRead size={30} />
-								<br />
-								최신 글
-							</button>
-							{/* </Link> */}
-							{/* <Link to="/mylogs"> */}
-							<button className={classes.btn}>
-								<AiOutlineFileSearch size={30} />
-								<br />내 글 보기
-							</button>
-							{/* </Link> */}
-							{/* <Link to="/fixprofile"> */}
-							<button className={classes.btn}>
-								<RiUserSettingsLine size={30} />
-								<br />
-								정보 수정
-							</button>
-							{/* </Link> */}
-						</div>
-					</div>
+		<div className="my-page-card-container">
+			<div className="my-page-card-image-container">
+				<img src={require('../../assets/svg/16.svg').default} alt="" height="100px" width="100px" />
+			</div>
+			<div className="my-page-card-info-container">
+				<h1>{nickname}</h1>
+				<h2>{email}</h2>
+				<button className="my-page-card-info-btn">
+					<FiPaperclip size={30} />
+				</button>
+				<div className="my-page-card-info-welcome-container">
+					<p>Welcome! What would you like to write today?</p>
 				</div>
 			</div>
-			)
-		</>
+			<div className="my-page-card-info-options">
+				<button>Moment</button>
+				<button>My post</button>
+				<button>Infomation</button>
+			</div>
+		</div>
 	);
 };
 
