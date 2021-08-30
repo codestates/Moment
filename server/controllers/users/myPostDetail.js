2b257011c1b91810538d921a84ee6dcb49889626
lotus0204 <81801012+lotus0204@users.noreply.github.com>
2021-08-30 11:34:26 +0900

    [Server]Add:myPost,myPostDetail / Fix: like, detail,logout (#140)
    * [Server] Add: login.js
    
    * [Server]Add:logout.js
    
    * [Server]Fix:login.js
    
    * [Server]Add:signup.js
    
    * [Server]fix:login,signup
    
    * [Server]Add:profile.js
    
    * [Server]Add:lib/index.js
    
    * [Server]Fix:login,signup,profile
    
    * [Server]Add:recent.js
    
    * [Server]fix:fix,submit
    
    * [Server]delete:file
    
    * [Server]fix:index.js..
    
    * [Server]fix:fixmyprofile
    
    * [Server]fix:liks.js
    
    * [Server]fix:detail
    
    * [Server]fix:like.js
    
    * [Sever]fix:like.js
    
    * [Server]fix:like.js
    
    * [Server]fix:like.js
    
    * [Server]fix:delete.js,fix.js,recent.js
    
    * [Server]fix:recent.js
    
    * [Server]Add:myPost.js,myPostDetail.js
    
    * [Server]Fix:detail.js,like.js
    
    * [Server]Fix:mypostdetail

---
+++ server/controllers/users/myPostDetail.js
@@ -0,0 +1,35 @@
+const { Posts, Users } = require('../../models');
+
+module.exports = async (req, res) => {
+  const post_id = req.params.id;
+  const accessToken = req.cookies.accessToken;
+  if (accessToken === undefined) {
+    res.status(400).json({ data: null });
+  } else {
+    const accessTokenData = isAuthorized(accessToken);
+    if (!accessTokenData) res.status(401).send({ data: null });
+    else {
+      try {
+        const post = await Posts.findByPk(post_id);
+        const userInfo = await Users.findByPk(post.user_id);
+        if (!post) {
+          res.status(404).json({ data: null });
+        } else {
+          const { id, title, content, updatedAt, like_count } = post;
+          res.status(200).json({
+            data: {
+              id,
+              title,
+              content,
+              author: userInfo.nickname,
+              updated: updatedAt,
+              like_count,
+            },
+          });
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }
+};
