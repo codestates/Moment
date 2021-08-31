a0a249c96ec3a7309a6be89e71a62e7642e3d3ef
Mangpha <8350130@gmail.co>
2021-08-31 15:18:00 +0900

    [Client] Fix: Conflict

--- client/src/Context.js
+++ client/src/Context.js
@@ -66,6 +66,7 @@ const ContextProvider = ({ children }) => {
 	const [posts, setPosts] = useState('');
 	useEffect(async () => {
 		const res = await axios.get(`${ENDPOINT}/log/recent/page/1`);
+		console.log(res.data.data.rows);
 		const logs = res.data.data.rows;
 		setPosts([...logs]);
 		console.log(posts);
