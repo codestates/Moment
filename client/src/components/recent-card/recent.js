3f242407b303fe1015f9aa0a2a3b8cd501dbc255
Mark <notbibu@gmail.com>
2021-09-01 09:46:35 +0900

    [Client]Add: backbtn

--- client/src/components/recent-card/recent.js
+++ client/src/components/recent-card/recent.js
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { TiHeartOutline } from 'react-icons/ti';
 import './recent.css';
-const RecentCard = ({ title, content, author, updated }) => {
+const RecentCard = ({ id, title, content, nickname, updated }) => {
 	const [click, setClick] = useState(false);
 	const [count, setCount] = useState(Number(1));
 	const clickHandler = () => {
 		setClick(!click);
 	};
+
+	const time = String(updated);
+	const idx = time.indexOf('T');
+	const t = time.slice(0, idx);
 
 	const countHandler = () => {
 		if (!click) {
@@ -18,18 +22,33 @@ const RecentCard = ({ title, content, author, updated }) => {
 		console.log(count);
 	};
 
+	const [randomNum, setRandomNum] = useState(1);
+	// 랜덤 수
+	const getRandomPic = () => {
+		let ranNum = Math.floor(Math.random() * 12) + 1;
+		setRandomNum(ranNum);
+	};
+
+	useEffect(() => {
+		getRandomPic();
+	}, []);
+
+	const detailHandler = () => {
+		document.location.href = `/log/detail/${id}`;
+	};
+
 	return (
 		<>
-			<div className="contains">
+			<div className="contains" onClick={detailHandler}>
 				<div className="recent-container">
 					<div className="recent-image-container">
-						<img src={require('../../static/images/composition-10.png').default} />
+						<img src={require(`../../static/images/composition-${randomNum}.png`).default} />
 					</div>
 					<div className="recent-card-container">
 						<div className="recent-title">
-							<h1>{title}</h1>
-							<span className="recent-card-nickname">{author}</span>
-							<span className="recent-card-updete">{updated}</span>
+							<p>{title}</p>
+							<span className="recent-card-nickname">{nickname}</span>
+							<span className="recent-card-updete">{t}</span>
 							<button
 								className={click ? `recent-card-liked-click` : 'recent-card-liked'}
 								onClick={clickHandler}
